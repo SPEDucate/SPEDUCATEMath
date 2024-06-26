1c6f7de6b482eaf6ee7c1cc006be425da0d73b69
Nathan <71136851+arandrollcode@users.noreply.github.com>
2024-06-25 20:41:16 -0500

    renamed to splash-screen

---
+++ screens/splash-screen.js
@@ -0,0 +1,26 @@
+import { Image, StyleSheet, View } from "react-native";
+import Icon from "../assets/splash-screen.png";
+
+export default function SplashScreen() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Image source={Icon} style={styles.image} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1B0F18",
+  },
+  image: {
+    width: 370,
+    height: 1000,
+    resizeMode: "cover",
+  },
+});
