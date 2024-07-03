bff66da9b8a3b2c8fc9b9a4899568a93522acc2e
Nathan <71136851+arandrollcode@users.noreply.github.com>
2024-07-02 22:30:45 -0500

    displays basic content

--- screens/question.js
+++ screens/question.js
@@ -2,9 +2,15 @@ import React, { useState, useRef } from "react";
 import { View, Text } from "react-native";
 
 const Question = (props) => {
+  console.log(props);
+
   return (
     <View>
-      <Text>{props.questionText}</Text>
+      <Text>{props.children.questionText}</Text>
+
+      {props.children.options.map((item, index) => (
+        <Text key={index}>{item}</Text>
+      ))}
     </View>
   );
 };
