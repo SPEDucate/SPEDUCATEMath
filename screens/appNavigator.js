3b6fc848f2c849fc57d4de624bc5b75a131f87a1
Nathan <71136851+arandrollcode@users.noreply.github.com>
2024-08-03 17:43:01 -0500

    changed tab size to 2

--- screens/appNavigator.js
+++ screens/appNavigator.js
@@ -13,50 +13,50 @@ import FiveMath from '../screens/5Math'; // Adjust path if needed
 const Stack = createStackNavigator();
 
 export default function AppNavigator() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
-                <Stack.Screen
-                    name="Login"
-                    component={Login}
-                    options={{ headerShown: false }} // Hide header for Login screen
-                />
-                <Stack.Screen
-                    name="Home"
-                    component={Home}
-                    options={{ headerShown: false }} // Hide header for Home screen
-                />
-                <Stack.Screen
-                    name="Kindergarten math"
-                    component={KMath}
-                    options={{ headerShown: false }} // Hide header for Kindergarten math screen
-                />
-                <Stack.Screen
-                    name="1st grade math"
-                    component={OneMath}
-                    options={{ headerShown: false }} // Hide header for 1st grade math screen
-                />
-                <Stack.Screen
-                    name="2nd grade math"
-                    component={TwoMath}
-                    options={{ headerShown: false }} // Hide header for 2nd grade math screen
-                />
-                <Stack.Screen
-                    name="3rd grade math"
-                    component={ThreeMath}
-                    options={{ headerShown: false }} // Hide header for 3rd grade math screen
-                />
-                <Stack.Screen
-                    name="4th grade math"
-                    component={FourMath}
-                    options={{ headerShown: false }} // Hide header for 4th grade math screen
-                />
-                <Stack.Screen
-                    name="5th grade math"
-                    component={FiveMath}
-                    options={{ headerShown: false }} // Hide header for 5th grade math screen
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }} // Hide header for Login screen
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }} // Hide header for Home screen
+        />
+        <Stack.Screen
+          name="Kindergarten math"
+          component={KMath}
+          options={{ headerShown: false }} // Hide header for Kindergarten math screen
+        />
+        <Stack.Screen
+          name="1st grade math"
+          component={OneMath}
+          options={{ headerShown: false }} // Hide header for 1st grade math screen
+        />
+        <Stack.Screen
+          name="2nd grade math"
+          component={TwoMath}
+          options={{ headerShown: false }} // Hide header for 2nd grade math screen
+        />
+        <Stack.Screen
+          name="3rd grade math"
+          component={ThreeMath}
+          options={{ headerShown: false }} // Hide header for 3rd grade math screen
+        />
+        <Stack.Screen
+          name="4th grade math"
+          component={FourMath}
+          options={{ headerShown: false }} // Hide header for 4th grade math screen
+        />
+        <Stack.Screen
+          name="5th grade math"
+          component={FiveMath}
+          options={{ headerShown: false }} // Hide header for 5th grade math screen
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
