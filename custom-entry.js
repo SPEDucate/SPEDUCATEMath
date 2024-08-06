b3d0c8c4ebd9157116f5f903bc39b1956bf151b3
Nathan <71136851+arandrollcode@users.noreply.github.com>
2024-08-05 21:42:12 -0500

    custom entry for ease of dev testing

---
+++ custom-entry.js
@@ -0,0 +1,9 @@
+import { registerRootComponent } from 'expo';
+
+// FOR TESTING A SPECIFIC PAGE, CHANGE THE ITEMS BELOW
+import PreferenceFormUI from './screens/preference-form';
+registerRootComponent(PreferenceFormUI);
+
+// FOR NORMAL USAGE, UNCOMMENT THIS
+// import App from './App'
+// registerRootComponent(App);
