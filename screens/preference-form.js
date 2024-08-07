85540a3f405ce369754aa54f960056fdad5cf31b
Nathan <71136851+arandrollcode@users.noreply.github.com>
2024-08-07 17:41:27 -0500

    read from csv (doesn't work)

--- screens/preference-form.js
+++ screens/preference-form.js
@@ -1,9 +1,30 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet, Alert } from "react-native";
+import Papa from "papaparse";
+import * as FileSystem from 'expo-file-system';
+
+async function getQuestionData() {
+  const relative_path = "data/".concat("preference form questions - Sheet1.csv");
+
+  const fileUrl = FileSystem.documentDirectory + relative_path;
+  console.log(fileUrl)
+  const fileContent = await FileSystem.readAsStringAsync(fileUrl);
+  console.log(fileContent);
+
+  // let res = Papa.parse(fileContent, {
+  //   header: true,
+  //   skipEmptyLines: true,
+  //   complete: (results) => {
+  //     console.log(results);
+  //   }
+  // });
+
+  // return res;
+}
 
 const PreferenceFormUI = () => {
   // Get the JSON file with all the questions info
-  const formData = require("../data/preference-form-data.json");
+  const formData = getQuestionData();
   // console.log(formData);
 
   const [questionIndex, setQuestionIndex] = useState(0);
@@ -32,7 +53,7 @@ const PreferenceFormUI = () => {
 
       {/* Answer Choices */}
       {currQuestionData.options.map((item, index) => (
-        <Text 
+        <Text
           key={index} onPress={incrementQuestion}>{item}</Text>
       ))}
     </View>
