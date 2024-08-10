0d54d046b3a36392f0a6ad96abbd89b9deee22bf
Nathan <pianobear2008@gmail.com>
2024-08-10 14:58:27 -0500

    a

--- custom-entry.js
+++ custom-entry.js
@@ -1,9 +1,9 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
 
 // FOR TESTING A SPECIFIC PAGE, CHANGE THE ITEMS BELOW
-import PreferenceFormUI from './screens/preference-form';
-registerRootComponent(PreferenceFormUI);
+// import PreferenceFormUI from './screens/preference-form';
+// registerRootComponent(PreferenceFormUI);
 
 // FOR NORMAL USAGE, UNCOMMENT THIS
-// import App from './App'
-// registerRootComponent(App);
+import App from "./App";
+registerRootComponent(App);
