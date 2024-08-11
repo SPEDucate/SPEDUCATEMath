edac568a46b4af20b0f4c33c3adbc69d8b7581a6
Nathan <pianobear2008@gmail.com>
2024-08-11 17:33:11 -0500

    transfer devices for dev

--- App.js
+++ App.js
@@ -1,49 +1,53 @@
 // App.js
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import SplashScreen from './screens/splash-screen'; // Adjust path if needed
-import AppNavigator from './screens/app-navigator'; // Adjust path if needed
-import { SQLiteProvider } from 'expo-sqlite';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import SplashScreen from "./screens/splash-screen"; // Adjust path if needed
+import AppNavigator from "./screens/app-navigator"; // Adjust path if needed
+import { SQLiteProvider } from "expo-sqlite";
 
 // Initialize the database
-const initializeDatabase = async (db) => {
-    try {
-        await db.execAsync(`
-           CREATE TABLE IF NOT EXISTS users (
-                id       INTEGER         PRIMARY KEY AUTOINCREMENT,
-                username VARCHAR         UNIQUE
-                  NOT NULL,
-                password VARCHAR (8, 20) NOT NULL
-            );
-        `);
-        console.log('Database initialized!');
-    } catch (error) {
-        console.log('Error while initializing the database:', error);
-    }
-};
+// const initializeDatabase = async (db) => {
+//     try {
+//         await db.execAsync(`
+//            CREATE TABLE IF NOT EXISTS users (
+//                 id       INTEGER         PRIMARY KEY AUTOINCREMENT,
+//                 username VARCHAR         UNIQUE
+//                   NOT NULL,
+//                 password VARCHAR (8, 20) NOT NULL
+//             );
+//         `);
+//         console.log('Database initialized!');
+//     } catch (error) {
+//         console.log('Error while initializing the database:', error);
+//     }
+// };
+
+//////////////////////////////// DATABASE STUFF ////////////////////////////////
+
+//////////////////////////////// DATABASE STUFF ////////////////////////////////
 
 const App = () => {
-    const [isShowSplashScreen, setIsShowSplashScreen] = useState(true);
+  const [isShowSplashScreen, setIsShowSplashScreen] = useState(true);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setIsShowSplashScreen(false);
-        }, 3000); // Show splash screen for 3 seconds
-    }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsShowSplashScreen(false);
+    }, 3000); // Show splash screen for 3 seconds
+  }, []);
 
-    return (
-        <SQLiteProvider databaseName="userDatabase.db" onInit={initializeDatabase}>
-            <View style={styles.container}>
-                {isShowSplashScreen ? <SplashScreen /> : <AppNavigator />}
-            </View>
-        </SQLiteProvider>
-    );
+  return (
+    <SQLiteProvider databaseName='userDatabase.db' onInit={initializeDatabase}>
+      <View style={styles.container}>
+        {isShowSplashScreen ? <SplashScreen /> : <AppNavigator />}
+      </View>
+    </SQLiteProvider>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
+  container: {
+    flex: 1,
+  },
 });
 
 export default App;
