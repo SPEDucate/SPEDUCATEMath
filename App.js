dc72421d442047583e891dbc1782d4762142eaa1
Nathan <71136851+arandrollcode@users.noreply.github.com>
2024-09-01 17:33:46 -0500

    removed sqlite imports

--- App.js
+++ App.js
@@ -3,24 +3,6 @@ import React, { useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import SplashScreen from "./screens/splash-screen"; // Adjust path if needed
 import AppNavigator from "./screens/app-navigator"; // Adjust path if needed
-import { SQLiteProvider } from "expo-sqlite";
-
-// Initialize the database
-const initializeDatabase = async (db) => {
-  try {
-    await db.execAsync(`
-           CREATE TABLE IF NOT EXISTS users (
-                id       INTEGER         PRIMARY KEY AUTOINCREMENT,
-                username VARCHAR          UNIQUE
-                  NOT NULL,
-                password VARCHAR (8, 20) NOT NULL
-            );
-        `);
-    console.log("Database initialized!");
-  } catch (error) {
-    console.log("Error while initializing the database:", error);
-  }
-};
 
 const App = () => {
   const [isShowSplashScreen, setIsShowSplashScreen] = useState(true);
@@ -32,11 +14,9 @@ const App = () => {
   }, []);
 
   return (
-    <SQLiteProvider databaseName="userDatabase.db" onInit={initializeDatabase}>
-      <View style={styles.container}>
-        {isShowSplashScreen ? <SplashScreen /> : <AppNavigator />}
-      </View>
-    </SQLiteProvider>
+    <View style={styles.container}>
+      {isShowSplashScreen ? <SplashScreen /> : <AppNavigator />}
+    </View>
   );
 };
 
