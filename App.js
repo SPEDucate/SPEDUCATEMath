6c3f52e0fb8f349266eb9e397af0b3be46cc5c25
Nathan <71136851+arandrollcode@users.noreply.github.com>
2024-09-11 23:14:36 -0500

    move app-navigator.js to /scripts folder

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import SplashScreen from "./screens/splash-screen";
-import AppNavigator from "./screens/app-navigator";
+import AppNavigator from "./scripts/app-navigator";
 import PathChooser from "./screens/pathChooser";
 
 const App = () => {
